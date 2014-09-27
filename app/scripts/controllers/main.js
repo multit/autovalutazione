388dc3d1df87c6a4645bbc1ab1e8be5b7ccf77cb
multit <andrea.fiorelli@gmail.com>
2014-09-27 08:59:30 +0200

    Primo upload scaffolded angular app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('autovalutazioneApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
