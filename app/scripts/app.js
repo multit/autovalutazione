4f0fc1e88657e8d37dde577619a3b3f56c963388
multit <andrea.fiorelli@gmail.com>
2014-09-30 16:29:07 +0200

    Logica front end completa

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,5 +16,5 @@ angular.module('autovalutazioneApp', [
       .otherwise({
         redirectTo: '/'
       });
-  }).value('duScrollOffset', 0);
+  }).value('duScrollOffset', -400);
 
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,26 +1,55 @@
 'use strict';
 
 angular.module('autovalutazioneApp').
-	controller('MainCtrl', [ '$scope', '$log','$document',
-	function ($scope,$log,$document) {
+	controller('MainCtrl', [ '$scope', '$document',
+	function ($scope, $document) {
     
 		$scope.questioni = [
-			'La tua età',
-			'Hai sofferto  di malessere o importanti lesioni cutanee dopo esserti esposta al sole o aver fatto una lampada solare?',
-			'Perdi molti capelli o li perdi a ciocche?',
-			'Ti senti spesso esageratamente stanca? ',
-			'Hai periodi di febbre o febbricola inspiegata?',
-			'Hai notato la comparsa di “ghiandole” (linfonodi) ingrossate al collo, alle ascelle o all’inguine?',
-			'Hai sofferto o soffri di afte/ulcere in bocca o dentro al naso?',
-			'Hai mai notato gonfiore o avvertito dolore a livello di qualche articolazione in assenza di cause evidenti (ad esempio trauma)',
-			'Hai notato che le punte delle dita ( mani e/o piedi) diventano bianche e/o livide (cianotiche) come se non “arrivasse sangue” quando ti esponi al freddo o in occasione di grosse emozioni?'
+			{domanda:'Per iniziare indica gentilmente la tua fascia di età', risposte:['20-30','30-40','50+']},			
+			{domanda:'Hai sofferto  di malessere o importanti lesioni cutanee dopo esserti esposta al sole o aver fatto una lampada solare?',risposte:['SI','NO']},
+			// {domanda:'Perdi molti capelli o li perdi a ciocche?',risposte:['SI','NO']},
+			// {domanda:'Ti senti spesso esageratamente stanca? ',risposte:['SI','NO']},
+			// {domanda:'Hai periodi di febbre o febbricola inspiegata?',risposte:['SI','NO']},
+			// {domanda:'Hai notato la comparsa di “ghiandole” (linfonodi) ingrossate al collo, alle ascelle o all’inguine?',risposte:['SI','NO']},
+			// {domanda:'Hai sofferto o soffri di afte/ulcere in bocca o dentro al naso?',risposte:['SI','NO']},
+			// {domanda:'Hai mai notato gonfiore o avvertito dolore a livello di qualche articolazione in assenza di cause evidenti (ad esempio trauma)',risposte:['SI','NO']},
+			{domanda:'Hai notato che le punte delle dita ( mani e/o piedi) diventano bianche e/o livide (cianotiche) come se non “arrivasse sangue” quando ti esponi al freddo o in occasione di grosse emozioni?',risposte:['SI','NO']}
 		];
 
-		$scope.$log = $log;
+		$scope.currDomanda;
+
+
+		$scope.inizia = function() {
+			$scope.schedaNo=0;
+			$scope.scrollaTo('questionContainer');
+		}
+
+		$scope.showNextQuestion = function (idx) {
+			$scope.schedaNo = idx + 1;  // mostra la scheda successiva
+			var nextQuestion = 'scheda' + idx;
+			$scope.scrollaTo(nextQuestion);
+		};
+
+		$scope.updateQuestionsResults = function (idx,risposta) {
+			if (idx < (Object.keys($scope.questioni).length-1)) {  // Lunghezza delle key dell'oggetto parte da 1
+				console.log('Questione: '+ idx + '\nRisposta selezionata: ' + risposta);
+				} else {
+				$scope.mostraRisultati();	
+				console.log('Mostra i risultati' + Object.keys($scope.questioni).length);		
+			}			
+		};
+
+		$scope.mostraRisultati = function () {
+			$scope.risultatiTest = true;
+		}
+
+		$scope.update = function (idx,risposta) {
+			$scope.showNextQuestion(idx);
+			$scope.updateQuestionsResults(idx,risposta);
+		};
 
 		$scope.scrollaTo = function(elem) {
 			var currQuestion = angular.element(document.getElementById(elem));
-			//$log.log(elem);
 			$document.scrollToElementAnimated(currQuestion);
 		};
 
@@ -28,19 +57,5 @@ angular.module('autovalutazioneApp').
 			$document.scrollTopAnimated(0);
 		};
 
-		$scope.showNextQuestion = function (idx) {
-			//$log.log('test');
-			$scope.schedaNo = idx + 1;  // mostra la scheda successiva
-			var nextQuestion = 'scheda' + idx;
-			$scope.scrollaTo(nextQuestion);
-		};
 
-		$scope.updateQuestionsResults = function (idx,risposta) {
-			console.log('Questione: '+ idx + '\nRisposta selezionata: ' + risposta);
-		};
-		$scope.update = function (evt,idx,risposta) {
-			$log.log('event:' + evt.clientX);
-			$scope.showNextQuestion(idx);
-			$scope.updateQuestionsResults(idx,risposta);
-		};
 	}]);
